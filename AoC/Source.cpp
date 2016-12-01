23f051d78cfcaeef521e8c8e70f2e02c51e786d5
robot <robot>
2016-12-01 14:39:08 -0600

    Day01 commit

--- AoC/Source.cpp
+++ AoC/Source.cpp
@@ -3,7 +3,7 @@
 
 int main() {
 	/*
-	Day01* santas_helper = new Day01("D:/workspace/AoC/AoC/Day1.txt");
+	Day01* santas_helper = new Day01("D:/workspace/AoC/AoC/Day01.txt");
 	std::cout << santas_helper->Move() << std::endl;
 	*/
 	getchar();
